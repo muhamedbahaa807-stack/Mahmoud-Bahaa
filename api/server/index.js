6ac20891babbc64c6fe76b34fc17ccb5d382d2f1
muhamed_bahaa <muhamedbahaa807@gmail.com>
2026-08-04 05:07:28 +0300

    Update CORS configuration to allow multiple origins

--- api/server/index.js
+++ api/server/index.js
@@ -8,7 +8,9 @@ import mainRouter from './routes/mainRoutes.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:5173','https://eleman2.vercel.app/'],
+  credentials: true }));
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URL)
